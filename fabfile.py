20e8499de572096318822f7e0fed29a136286704
Howey Huo <huoyihy@hotmail.com>
2016-03-09 14:44:25 -0800

    Add retry on cloud-init purge command in fabic file. sleep 10 with each retry.

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,5 @@
+import time
+
 from fabric.api import *
 from fabric.contrib.files import exists, append, contains, sed, comment
 
@@ -8,4 +10,11 @@ def config_env(ip, key_path):
 	env.disable_known_hosts = True
 
 def remove_cloud_init():
-	sudo('apt-get purge -y cloud-init')
+	while True:
+		res = sudo('apt-get purge -y cloud-init')
+		if "out: E: Could not get lock /var/lib/dpkg/lock - open" in res:
+			print("Failed to get dpkg lock, wait 10 seconds and try again")
+			time.sleep(10)
+		else:
+			break;
+
